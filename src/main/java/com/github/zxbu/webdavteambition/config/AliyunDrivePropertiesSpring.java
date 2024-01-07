8c5be24ac700e398761d94c9a444038927f60902
Jason <eritpchy@qq.com>
2024-01-07 12:12:25 +0800

    🐛Workaround for working dir not writeable

--- src/main/java/com/github/zxbu/webdavteambition/config/AliyunDrivePropertiesSpring.java
+++ src/main/java/com/github/zxbu/webdavteambition/config/AliyunDrivePropertiesSpring.java
@@ -1,12 +1,15 @@
 package com.github.zxbu.webdavteambition.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.io.File;
 import java.util.UUID;
 
+@Slf4j
 @ConfigurationProperties("aliyundrive")
 public class AliyunDrivePropertiesSpring extends AliyunDriveProperties implements InitializingBean  {
 
@@ -14,8 +17,9 @@ public class AliyunDrivePropertiesSpring extends AliyunDriveProperties implement
     public void afterPropertiesSet() throws Exception {
         String refreshToken = this.getRefreshToken();
         Auth auth = this.getAuth();
-        AliyunDriveProperties other = load(getWorkDir());
-        other.setWorkDir(getWorkDir());
+        String workDir = ensureWorkDir(getWorkDir());
+        AliyunDriveProperties other = load(workDir);
+        other.setWorkDir(workDir);
         other.setDriver(getDriver());
         other.setDownloadProxyMode(getDownloadProxyMode());
         other.setShareExpireSec(getShareExpireSec());
@@ -40,5 +44,34 @@ public class AliyunDrivePropertiesSpring extends AliyunDriveProperties implement
             this.setShareToken(UUID.randomUUID().toString().replace("-", "").substring(0, 24));
         }
         save();
+    }
+
+    private String ensureWorkDir(String workPath) {
+        File workDir = new File(workPath);
+        if (!workDir.exists()) {
+            try {
+                workDir.mkdirs();
+            } catch (Exception e) {
+                log.error("ensureWorkDir, create dir failed", e);
+            }
+        }
+        if (workDir.exists() && workDir.isDirectory()) {
+            return workDir.getAbsolutePath() + File.separator;
+        }
+        workDir = new File(System.getProperty("user.home"), ".aliyundrive");
+        if (!workDir.exists()) {
+            try {
+                workDir.mkdirs();
+            } catch (Exception e) {
+                log.error("ensureWorkDir, create dir failed", e);
+            }
+        }
+        if (workDir.exists() && workDir.isDirectory()) {
+            String newWorkPath = workDir.getAbsolutePath() + File.separator;
+            log.warn("Default workDir({}) is not writeable, change to {} instead",
+                    new File(workPath).getAbsoluteFile() + File.separator, newWorkPath);
+            return newWorkPath;
+        }
+        return workPath;
     }
 }
