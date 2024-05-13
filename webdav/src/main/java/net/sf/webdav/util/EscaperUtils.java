63b0de98f682be24ff8def97730f52c9c21cc6cb
Jason <eritpchy@qq.com>
2024-05-13 22:55:49 +0800

    🐛Fixed public link issue

--- webdav/src/main/java/net/sf/webdav/util/EscaperUtils.java
+++ webdav/src/main/java/net/sf/webdav/util/EscaperUtils.java
@@ -4,8 +4,17 @@ import net.sf.webdav.fromcatalina.URLEncoder;
 
 public class EscaperUtils {
 
-    private static URLEncoder URL_Encoder = new URLEncoder();
+    private static URLEncoder URL_ENCODER = new URLEncoder();
+
+    static {
+        URL_ENCODER.addSafeCharacter('-');
+        URL_ENCODER.addSafeCharacter('_');
+        URL_ENCODER.addSafeCharacter('.');
+        URL_ENCODER.addSafeCharacter('/');
+    }
+
     public static String escapePath(String s) {
-        return URL_Encoder.encode(s).replace("&", "%26");
+        return URL_ENCODER.encode(s)
+                .replace("&", "%26");
     }
 }
